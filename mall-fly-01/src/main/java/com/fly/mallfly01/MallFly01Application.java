6cf7168fa285fabc0e04bfdd266115d5cc4c8ff8
FLY <964747264@qq.com>
2022-09-27 15:54:19 +0800

    feat: fly-01

---
+++ mall-fly-01/src/main/java/com/fly/mallfly01/MallFly01Application.java
@@ -0,0 +1,13 @@
+package com.fly.mallfly01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFly01Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MallFly01Application.class, args);
+    }
+    
+}
---
+++ mall-fly-01/src/test/java/com/fly/mallfly01/MallFly01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.mallfly01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallFly01ApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
